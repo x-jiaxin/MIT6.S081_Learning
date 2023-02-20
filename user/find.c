c55215a8a8c9c33ccdef9c210a99ddb426d5d478
幻想 <jiaxin.cuckoo@gmail.com>
2023-02-20 16:05:08 +0800

    123

--- user/find.c
+++ user/find.c
@@ -1,18 +1,71 @@
+#include "kernel/fcntl.h"
 #include "kernel/fs.h"
 #include "kernel/stat.h"
 #include "kernel/types.h"
 #include "user/user.h"
 
-void find(char *path, char *file_name) {
-  struct dirent dt;
-  struct stat st;
+void find(char *path, char *file_name)
+{
+    struct dirent dt;
+    struct stat st;
+    char buf[512], *p;
+    int fd = open(path, 0);
+    if (fd < 0) {
+        fprintf(2, "open dir failed!\n");
+        exit(1);
+    }
+    if (fstat(fd, &st) < 0) {
+        fprintf(2, "stat dir failed!\n");
+        close(fd);
+        exit(1);
+    }
+    if (st.type != T_DIR) {
+        fprintf(2, "not a dir\n");
+        exit(1);
+    }
+
+    strcpy(buf, path);
+    p = buf + strlen(path);
+    *p++ = '/';
+    while (read(fd, &dt, sizeof(dt)) == sizeof(dt)) {
+        if (dt.inum == 0) {
+            continue;
+        }
+        memmove(p, dt.name, DIRSIZ);
+        p[DIRSIZ] = 0;
+        if (stat(buf, &st) < 0) {
+            continue;
+        }
+        switch (st.type) {
+            case T_FILE:
+                if (strcmp(p, file_name) == 0) {
+                    fprintf(1, "%s\n", buf);
+                }
+                break;
+            case T_DIR:
+                if (strcmp(p, ".") != 0 && strcmp(p, "..") != 0) {
+                    find(buf, file_name);
+                }
+                break;
+        }
+    }
+    close(fd);
 }
 
-int main(int argc, char *argv[]) {
-  if (argc != 3) {
-    fprintf(2, "Usage: find path filename\n");
-    exit(1);
-  }
-  find(argv[1], argv[2]);
-  exit(0);
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        if (argc == 2) {
+            find(".", argv[1]);
+            exit(0);
+        }
+        else {
+            fprintf(2, "Usage: find path filename\n");
+            exit(1);
+        }
+    }
+    else {
+        find(argv[1], argv[2]);
+        exit(0);
+    }
 }
--- user/xargs.c
+++ user/xargs.c
@@ -12,34 +12,37 @@
 argv[1]:echo
 argv[2]:line
  */
-int main(int argc, char *argv[]) {
-  sleep(10);
-  char buf[SIZE];
-  char *xargv[SIZE];
-  int xargc = 0;
-  char *command = argv[1];
-  for (int i = 1; i < argc; ++i) {
-    xargv[xargc++] = argv[i];
-  }
-  while (read(0, buf, SIZE) > 0) {
-    if (fork() == 0) {
-      char *p = (char *)malloc(sizeof(buf));
-      int pi = 0;
-      for (int i = 0; i < sizeof(buf); ++i) {
-        if (buf[i] == ' ' || buf[i] == '\n') {
-          xargv[xargc++] = p;
-          pi = 0;
-          p = (char *)malloc(sizeof(buf));
-        } else {
-          p[pi++] = buf[i];
-        }
-      }
-      free(p);
-      xargv[xargc] = 0;
-      exec(command, xargv);
-    } else {
-      wait(0);
+int main(int argc, char *argv[])
+{
+    sleep(10);
+    char buf[SIZE];
+    char *xargv[SIZE];
+    int xargc = 0;
+    char *command = argv[1];
+    for (int i = 1; i < argc; ++i) {
+        xargv[xargc++] = argv[i];
     }
-  }
-  exit(0);
+    while (read(0, buf, SIZE) > 0) {
+        if (fork() == 0) {
+            char *p = (char *)malloc(sizeof(buf));
+            int pi = 0;
+            for (int i = 0; i < sizeof(buf); ++i) {
+                if (buf[i] == ' ' || buf[i] == '\n') {
+                    xargv[xargc++] = p;
+                    pi = 0;
+                    p = (char *)malloc(sizeof(buf));
+                }
+                else {
+                    p[pi++] = buf[i];
+                }
+            }
+            free(p);
+            xargv[xargc] = 0;
+            exec(command, xargv);
+        }
+        else {
+            wait(0);
+        }
+    }
+    exit(0);
 }
